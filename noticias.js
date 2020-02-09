c7f35e4a2f27705ff62b4aefc1d195aed948e69c
ale182 <alestalves@gmail.com>
2020-02-09 00:28:20 -0300

    Respondendo requisições HTTP

--- noticias.js
+++ noticias.js
@@ -1,1 +1,18 @@
+// require incorpora outros arquivos no arquivo atual, tipo 'include'
+// pode importar modulo, biblioteca, outras paginas...
+var http = require('http');
+
+// criando um 'servidor' para responder requisicao HTTP
+
+var server = http.createServer( function(req,res){
+// dentro do servidor
+
+// retorna um codigo html
+res.end("<html><body>Portal de Noticias</body></html>");
+
+});
+
+// fica 'escutando' a porta 3000
+server.listen(3000);
+
 console.log('teste');
