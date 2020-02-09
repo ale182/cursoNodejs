a97dd7cb500eefefba27d1cff1e93d252cedc85e
ale182 <alestalves@gmail.com>
2020-02-09 00:41:31 -0300

    Respondendo requisições com base na url requisitada

--- noticias.js
+++ noticias.js
@@ -8,7 +8,25 @@ var server = http.createServer( function(req,res){
 // dentro do servidor
 
 // retorna um codigo html
-res.end("<html><body>Portal de Noticias</body></html>");
+// res.end("<html><body>Portal de Noticias</body></html>");
+
+// lendo a URL passada e retornando HTML de acordo com o endereco
+var categoria = req.url ;
+
+if (categoria == '/tecnologia'){
+    res.end("<html><body>Noticias de Tecnologia</body></html>");    
+
+} else if (categoria == '/moda'){
+    res.end("<html><body>Noticias de Moda</body></html>");
+
+} else if (categoria == '/beleza'){
+    res.end("<html><body>Noticias de Beleza</body></html>");
+
+} else {
+    res.end("<html><body>Portal de Noticias</body></html>");
+
+}
+
 
 });
 
