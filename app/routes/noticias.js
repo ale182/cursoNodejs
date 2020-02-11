6f9020c5e7c3e50d597000dba2ce816c3cbc61dc
ale182 <alestalves@gmail.com>
2020-02-11 01:36:27 -0300

    24. Criando a conexão com o MySQL e recuperando dados

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,23 @@
 module.exports = function(app){
     app.get('/noticias' , function(req , res){
-        res.render("noticias/noticias");    
-    });
-};
+
+        var mysql = require('mysql') ;
+
+        var connection  = mysql.createConnection({
+            host     : '192.168.200.106' ,
+            user     : 'root' ,
+            password : '123456',
+            database : 'portal_noticias'
+
+        });
+
+
+        connection.query('select * from noticias',function(error,result){
+                res.send(result);
+            });
+
+
+        
+            // res.render("noticias/noticias");    
+        });
+    };
