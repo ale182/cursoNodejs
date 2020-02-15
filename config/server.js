b0f23b4753adc0f6f8e21acc7510b6d90718bbb6
ale182 <alestalves@gmail.com>
2020-02-15 18:26:31 -0300

    38. Recebendo dados de formulários via POST com Body-parser

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var consign = require('consign');
-
+var bodyParser = require('body-parser');
 
 
 
@@ -19,9 +19,11 @@ app.set('view engine','ejs');
 // setar o caminho de views padrao
 app.set('views','./app/views');
 
+//implementando o bodyparser , { implementa atraves de json as urls codificadas }
+app.use(bodyParser.urlencoded({extended:true}));
+
 // inclui o diretorio das views para carregar automaticamente e fazer
 // os 'requires' de forma automatica
-                         
 consign()
     .include('app/routes') // inclui as rotas dentro do 'servidor'
     .then('config/dbConnection.js') // inclui o carregamento do dbConnection ao carregar as rotas
